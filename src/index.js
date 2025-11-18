c3ca12f91955fd431bc3bed816bc329145e64c00
mainlyti <mainlyti@gmail.com>
2025-11-17 23:18:22 -0500

    added bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import SearchWeather from "./SearchWeather";
 import reportWebVitals from "./reportWebVitals";
+import bootstrap from "bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
