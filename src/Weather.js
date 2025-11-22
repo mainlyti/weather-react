12ea41dc172aaabc4afc11480761afe12a19d330
mainlyti <mainlyti@gmail.com>
2025-11-22 18:17:57 -0500

    added api url and in process of getting data

--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setReady(true);
+  }
+
+  let apiKey = "5bfa080034fb64td6b864b3a813efo04";
+  let city = "New York";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(handleResponse);
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              ></input>
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-secondary w-100"
+              ></input>
+            </div>
+          </div>
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Saturday 4:44 PM</li>
+          <li>Cloudy</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-              autoFocus="on"
-            ></input>
+          <div className="col-6">
+            <p>Img goes here</p>
+            <span className="temperature"></span>
+            <span className="unit">°F</span>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-secondary w-100"
-            ></input>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 3 mph</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Saturday 4:44 PM</li>
-        <li>Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <p>Img goes here</p>
-          <span className="temperature">45</span>
-          <span className="unit">°F</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 3 mph</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return "Loading..";
+  }
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import Weather from "./Weather";
 import "./Weather.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
